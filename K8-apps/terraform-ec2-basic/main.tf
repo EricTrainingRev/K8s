0df3f257eb53f596ef138bf58f7439e44778a8c4
EricTrainingRev <eric.suminski@revature.com>
2023-01-12 18:11:05 -0500

    added information to set terraform state in remote location

--- K8-apps/terraform-ec2-basic/main.tf
+++ K8-apps/terraform-ec2-basic/main.tf
@@ -20,3 +20,36 @@ module "ec2-instance" {
     Environment = "dev"
   }
 }
+
+# this bucket will be used to store terraform state
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "revature-example-state-bucket"
+  # Prevent accidental deletion of this S3 bucket
+  lifecycle {
+    prevent_destroy = true
+  }
+  # Enable versioning so we can see the full revision history of our
+  # state files
+  versioning {
+    enabled = true
+  }
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+# this dynamodb instance helps manage users interacting with the terraform state at the same time
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "demo-terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
--- K8-apps/terraform-ec2-basic/terraform.tf
+++ K8-apps/terraform-ec2-basic/terraform.tf
@@ -5,4 +5,13 @@ terraform {
       version = "~> 4.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "revature-example-state-bucket"
+    key            = "state/terraform.tfstate"
+    region         = "us-east-1"
+    profile        = "demo-user" # can be set in .aws/config document
+    dynamodb_table = "demo-terraform-locks"
+    encrypt        = true
+  }
 }
